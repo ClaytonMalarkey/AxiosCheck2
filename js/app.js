d0db44e40ef574f9e0f2552a78c494518de83c34
ClaytonMalarkey <malarkey1985@gmail.com>
2023-10-03 18:26:47 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+let databody = document.body;
+
+function successFunction(response) {
+    // Do success code here
+
+    console.log(response);
+    const datafromResponse= response.meals;
+    diplaydata(datafromResponse);
+
+}
+function failureFunction(error) {
+    // Do failure code here
+    console.log('failure');
+    databody.insertAdjacentHTML('afterend', '<h3>no luck loading</h3>');
+}
+
+function diplaydata(datafromResponse) {
+    
+    console.log(datafromResponse);  
+    console.log(datafromResponse.length);
+
+    let counter = 0;
+    while (counter < datafromResponse.length ){
+        let post = datafromResponse[counter];
+        databody.insertAdjacentHTML('afterend',`<h4>${post.strMeal}</h4><h3>${post.strArea}</h3><p>${post.strInstructions}</p>`);
+        counter = counter + 1;
+    }
+}
+
+axios.request({
+    url: `www.themealdb.com/api/json/v1/1/random.php`
+}).then(successFunction).catch(failureFunction);
